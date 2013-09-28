738c64ea6cf46eddaa08d11f6bbb9a3f9de59977
Jon <jonchen42@gmail.com>
2013-09-28 17:18:23 -0400

    making final changes

--- popup.js
+++ popup.js
@@ -1,3 +1,40 @@
+/* popup.js */
+
 function displayPopup() {
-	
+	// var popover3 = document.createElement('a');
+	// 	popover3.setAttribute('class', 'btn btn-success');
+	// 	popover3.setAttribute('data-toggle', 'popover');
+	// 	popover3.setAttribute('href', '#');
+	// 	popover3.setAttribute('data-content', 'Test');
+	// 	popover3.setAttribute('data-original-title', 'Bootstrap Popover Test');
+
+	//var pic = document.getElementById('jobtitle1');
+		// document.body.appendChild(popover3);
+		//popover.setAttribute('data-original-title', 'This is a test');
+
+	/* var options = {
+		html:true,
+		placement:'auto',
+		trigger:'click',
+		title:'Test',
+		content:'Test content'
+	} */
+
+	//$(popover).popover({placement:'top'});
+
+/* <div id="myCarousel" class="carousel slide">
+               <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+              <div class="active item">…</div>
+    <div class="item">…</div>
+    <div class="item">…</div>
+    </div>
+    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+    </div>" */
+
 }
